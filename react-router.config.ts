7de00cf4ee8aec0f62e5669f95faeea070706ce0
YosriMlik <ymlik24@gmail.com>
2025-11-05 19:11:44 +0100

    disable ssr

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,5 +2,8 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   // Disable SSR for true static site generation
-  ssr: false,
+  // ssr: false,
+  async prerender() {
+    return ["/", "/about", "/services", "/contact"];
+  },
 } satisfies Config;
